d3c5f1dd7eababc38dd1307c6d5fe7b01d0a22db
Adrien <necropotame@gmail.com>
2015-12-25 22:51:34 +0100

    Add example image

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1845,8 +1845,8 @@ bool CServer::CreateMod(const char* pModName)
 	str_format(aBuf, sizeof(aBuf), "mods/%s.mod", pModName);
 	
 	CModAPI_ModCreator ModCreator;
-	if(ModCreator.AddImage(Storage(), "modapi/weapons.png") != MODAPIEXAMPLE_IMAGE_WEAPONS) return false;
-	if(ModCreator.AddSpriteExternal(0, 1, 0, 1, 1, 16, 16) != MODAPIEXAMPLE_SPRITE_TARGET) return false;
+	if(ModCreator.AddImage(Storage(), "modapi/example.png") != MODAPIEXAMPLE_IMAGE_WEAPONS) return false;
+	if(ModCreator.AddSpriteExternal(0, 0, 0, 1, 1, 16, 16) != MODAPIEXAMPLE_SPRITE_TARGET) return false;
 	if(ModCreator.Save(Storage(), aBuf) < 0) return false;
 	
 	return true;
